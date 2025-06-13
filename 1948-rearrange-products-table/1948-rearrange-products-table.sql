1cd70e68b55a7104220c897ca963ba29602d9d0a
Afthashkhalid <khalidafthash@gmail.com>
2025-06-13 10:19:44 +0530

    Time: 373 ms (77.83%), Space: 0B (100%) - LeetHub

--- 1948-rearrange-products-table/1948-rearrange-products-table.sql
+++ 1948-rearrange-products-table/1948-rearrange-products-table.sql
@@ -1,11 +1,7 @@
 /* Write your T-SQL query statement below */
--- SELECT product_id, 'store1' AS store, store1 AS price FROM Products WHERE store1 IS NOT NULL
--- UNION ALL 
--- SELECT product_id, 'store2' AS store, store2 AS price FROM Products WHERE store2 IS NOT NULL
--- UNION ALL 
--- SELECT product_id, 'store3' AS store, store3 AS price FROM Products WHERE store3 IS NOT NULL;
+SELECT product_id, 'store1' AS store, store1 AS price FROM Products WHERE store1 IS NOT NULL
+UNION ALL 
+SELECT product_id, 'store2' AS store, store2 AS price FROM Products WHERE store2 IS NOT NULL
+UNION ALL 
+SELECT product_id, 'store3' AS store, store3 AS price FROM Products WHERE store3 IS NOT NULL;
 
-SELECT product_id, store, price FROM Products
-UNPIVOT(
-    price for store IN(store1, store2, store3)
-) AS unpvt;
