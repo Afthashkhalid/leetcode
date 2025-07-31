0ebb576ff3d83b06a0b68072ff28ffbd411150c5
Afthashkhalid <khalidafthash@gmail.com>
2025-07-31 08:39:49 +0530

    Time: 0 ms (100%), Space: 39.9 MB (66.8%) - LeetHub

---
+++ 0782-jewels-and-stones/0782-jewels-and-stones.cs
@@ -0,0 +1,11 @@
+public class Solution {
+   public int NumJewelsInStones(string jewels, string stones) {
+    int count = 0;
+    foreach (char stone in stones) {
+        if (jewels.Contains(stone)) {
+            count++;
+        }
+    }
+    return count;
+}
+}
